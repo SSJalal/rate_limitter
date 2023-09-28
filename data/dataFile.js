4c1a5f17fd6d803cc61e9de06e278f3e730543f1
SSJalal <34628798+SSJalal@users.noreply.github.com>
2023-09-28 21:44:41 +0530

    Update dataFile.js

--- data/dataFile.js
+++ data/dataFile.js
@@ -1,4 +1,4 @@
-exports.limit = 2
-exports.maxTime = 20000
+exports.limit = 2 //maximum number of request a single user can generate
+exports.maxTime = 20000 //maximum time a single request can take to complete
 exports.requestsArr = []
 exports.requestsObj = {}
